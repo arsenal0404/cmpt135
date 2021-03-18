aa6338ac906c2821216328a38c413de7db82b82a
epicSpark <justinchiang707@gmail.com>
2021-03-17 17:30:40 -0700

    updated assignment 4

--- assignments/135-spring-2021-assignment-4/a4.cpp
+++ assignments/135-spring-2021-assignment-4/a4.cpp
@@ -44,3 +44,94 @@ int sum_of_squares(int n) {
 		return n*n + sum_of_squares(n - 1);
 	}
 }
+
+
+int sum_strange(int n){
+	assert(n > 0);
+	if (n < 2){  // base case
+		return 0;
+	} else if (n % 3 == 0 && n % 5 != 0){
+		//adds one when condition is true
+		return 1 + sum_strange(n - 1);
+	} else {
+		//does nothing when condition is false
+		return sum_strange(n - 1);
+	}
+}
+
+bool all_same(const string& s){
+	if (s.size() <= 1){
+		return true;
+	} else if (s.at(0) == s.at(1)){
+		// returns a string that is from index 1 to end of original string
+		return all_same(s.substr(1)); 
+	} else {
+		return false;
+	}
+}
+
+bool all_digits(const string& s){
+	if (s.size() == 0){
+		return true;
+	} else if (s.at(0) >= '0' && s.at(0) <= '9'){
+		// returns a string that is from index 1 to end of original string
+		return all_digits(s.substr(1)); 
+	} else {
+		return false;
+	}
+}
+
+string trim(const string& s){
+
+
+
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////Tests//////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+void sum_of_squares_test(){
+	cout << "Testing sum_of_squares ... ";
+	assert(sum_of_squares(3) == 14);
+    assert(sum_of_squares(5) == 55);
+
+	cout << "all sum_of_squares tests passed!\n";
+}
+
+void sum_strange_test(){
+	cout << "Testing sum_strange ... ";
+	assert(sum_strange(20) == 5);
+	assert(sum_strange(30) == 8);
+	assert(sum_strange(50) == 13);
+	cout << "all sum_strange tests passed!\n";
+}
+
+void all_same_test(){
+	cout << "Testing all_same ... ";
+	assert(all_same("ssssss") == true);
+	assert(all_same("asdfa") == false);
+	assert(all_same("33333333333") == true);
+	assert(all_same("hji897") == false);
+	assert(all_same("") == true);
+	cout << "all all_same tests passed!\n";
+}
+
+void all_digits_test(){
+	cout << "Testing all_digits ... ";
+	assert(all_digits("234572345") == true);
+	assert(all_digits("") == true);
+	assert(all_digits("ajikhklaj") == false);
+	assert(all_digits("987h89789") == false);
+	cout << "all all_digits tests passed!\n";
+}
+
+void trim_test(){
+	cout << "Testing trim ... ";
+	assert(trim("  ab c  d ") == "ab c  d");
+	assert(trim("") == "");
+	assert(trim("   456 2 d 3 w asdf 23dg1      ") == "456 2 d 3 w asdf 23dg1");
+	cout << "all trim tests passed!\n";
+}
--- assignments/135-spring-2021-assignment-4/a4_main.cpp
+++ assignments/135-spring-2021-assignment-4/a4_main.cpp
@@ -6,10 +6,10 @@
 // re-write it any way you like. The marker will be using their own main
 // function.
 int main () {
-//     sum_of_squares_test();
-//     sum_strange_test();
-//     all_same_test();
-//     all_digits_test();
+    sum_of_squares_test();
+    sum_strange_test();
+    all_same_test();
+    all_digits_test();
 //     trim_test();
 //     sum_neg_test();
 //     min_vec_test();
